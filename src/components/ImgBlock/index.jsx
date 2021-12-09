dac4f1b92b3688b603a9ed4028bdf6ebadb8fa5a
Maria Rotolo <mariarotolo1994@gmail.com>
2021-12-09 20:09:48 +0100

    fix 1 stile

--- src/components/ImgBlock/index.jsx
+++ src/components/ImgBlock/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './style.css';
 
 const ImgBlock = (img) => (
-    <div className="product-img">
+    <div className="product_img">
         <img src={img.src} alt={img.alt} />
     </div>
 )
